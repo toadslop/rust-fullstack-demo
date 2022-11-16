8333e5f52e1a0c647efbaf04ef4b21c03c74d560
brian heise <bnheise@gmail.com>
2022-11-17 08:06:26 +0900

    moved url init functions to shared project

--- backend/src/config/env.rs
+++ backend/src/config/env.rs
@@ -1,8 +1,4 @@
-use shared::{
-    init_database_url, Url, BACKEND_HOST_KEY, BACKEND_PORT_KEY, BACKEND_PROTOCOL_KEY,
-    DEFAULT_BACKEND_URL, DEFAULT_FRONTEND_URL, FRONTEND_HOST_KEY, FRONTEND_PORT_KEY,
-    FRONTEND_PROTOCOL_KEY,
-};
+use shared::{init_backend_url, init_database_url, init_frontend_url, Url};
 
 #[derive(Debug, Clone)]
 pub struct Env {
@@ -14,9 +10,9 @@ pub struct Env {
 impl Env {
     pub fn init() -> Self {
         Self {
-            frontend_url: Self::init_frontend_url(),
+            frontend_url: init_frontend_url(),
             db_url: init_database_url(),
-            backend_url: Self::init_backend_url(),
+            backend_url: init_backend_url(),
         }
     }
 
@@ -30,49 +26,5 @@ impl Env {
 
     pub fn get_backend_url(&self) -> &Url {
         &self.backend_url
-    }
-
-    fn init_frontend_url() -> Url {
-        let mut url =
-            Url::parse(DEFAULT_FRONTEND_URL).expect("the default frontend url to be parseable");
-
-        if let Ok(protocol) = std::env::var(FRONTEND_PROTOCOL_KEY) {
-            url.set_scheme(&protocol).unwrap();
-        }
-
-        if let Ok(host) = std::env::var(FRONTEND_HOST_KEY) {
-            url.set_host(Some(&host)).unwrap();
-        }
-
-        if let Ok(port) = std::env::var(FRONTEND_PORT_KEY) {
-            let port = port
-                .parse::<u16>()
-                .expect("the frontend port to parseable to u16");
-            url.set_port(Some(port)).unwrap();
-        };
-
-        url
-    }
-
-    fn init_backend_url() -> Url {
-        let mut url =
-            Url::parse(DEFAULT_BACKEND_URL).expect("the default backend url to be parseable");
-
-        if let Ok(protocol) = std::env::var(BACKEND_PROTOCOL_KEY) {
-            url.set_scheme(&protocol).unwrap();
-        }
-
-        if let Ok(host) = std::env::var(BACKEND_HOST_KEY) {
-            url.set_host(Some(&host)).unwrap();
-        }
-
-        if let Ok(port) = std::env::var(BACKEND_PORT_KEY) {
-            let port = port
-                .parse::<u16>()
-                .expect("the frontend port to parseable to u16");
-            url.set_port(Some(port)).unwrap();
-        };
-
-        url
     }
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -61,4 +61,47 @@ pub fn init_database_url() -> Url {
     url
 }
 
+pub fn init_backend_url() -> Url {
+    let mut url = Url::parse(DEFAULT_BACKEND_URL).expect("the default backend url to be parseable");
+
+    if let Ok(protocol) = std::env::var(BACKEND_PROTOCOL_KEY) {
+        url.set_scheme(&protocol).unwrap();
+    }
+
+    if let Ok(host) = std::env::var(BACKEND_HOST_KEY) {
+        url.set_host(Some(&host)).unwrap();
+    }
+
+    if let Ok(port) = std::env::var(BACKEND_PORT_KEY) {
+        let port = port
+            .parse::<u16>()
+            .expect("the frontend port to parseable to u16");
+        url.set_port(Some(port)).unwrap();
+    };
+
+    url
+}
+
+pub fn init_frontend_url() -> Url {
+    let mut url =
+        Url::parse(DEFAULT_FRONTEND_URL).expect("the default frontend url to be parseable");
+
+    if let Ok(protocol) = std::env::var(FRONTEND_PROTOCOL_KEY) {
+        url.set_scheme(&protocol).unwrap();
+    }
+
+    if let Ok(host) = std::env::var(FRONTEND_HOST_KEY) {
+        url.set_host(Some(&host)).unwrap();
+    }
+
+    if let Ok(port) = std::env::var(FRONTEND_PORT_KEY) {
+        let port = port
+            .parse::<u16>()
+            .expect("the frontend port to parseable to u16");
+        url.set_port(Some(port)).unwrap();
+    };
+
+    url
+}
+
 pub use url::Url;
